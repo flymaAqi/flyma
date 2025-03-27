57aa5933c16b92651e074d7fa4a3c64fc5f388b2
Flyma-Aqi <104605941+flymaAqi@users.noreply.github.com>
2025-03-27 11:03:42 +0800

    语言优化

--- pagesB1/about/planOne/planOne.vue
+++ pagesB1/about/planOne/planOne.vue
@@ -19,97 +19,103 @@
 					<text style="color: #0055ff;">{{item.tit}}</text>
 				</view>
 			</view>
-			<view class="flex padding-lg">
-				<view class="text-content" style="letter-spacing: 0.1rem;">
-					<view class="margin-top-lg text-xl text-white">dddd</view>
-					<text :user-select="true">
-						&nbsp;&nbsp;如果你恰好有以上互联网业务开发需求，如果你想要一款量身定制的移动应用，如果你有新互联网项目计划，如果你恰好不知该怎么实行？
-						那么在你面前就有一个优质选择，首先我是一对一式专享服务，假如你是小白，我会分析你提出的互联网项目可行性，我会告诉你该项目最贴近现实的互联网环境，
-						我会告诉你一切你最需要知道甚至你不想听到的内容，我会尽力解答你所有的疑惑，并给出可行方案；
-					</text>
-					<view class="margin-top-sm">
-						&nbsp;&nbsp; 那话不多说，先看价格：
-					</view>
-				</view>
-			</view>
-			<view class="cu-coupon-box2 text-center align-center justify-center"
-				style="height: 60px;margin-top: -10px;">
-				<view class="flex align-center" style="height: 100%;">
-					<view class="flex-sub align-center">
-						<view class="grid col-1">
-							<view class="text-blue">
-								微信小程序:
-							</view>
-						</view>
-					</view>
-					<view class="flex-twice text-left">
-						<view class="grid col-1">
-							<view class="text-sm text-green">
-								简便·轻量·1百至8千元之间视需求不等；
-							</view>
+			<view class="aqi-txt-blod">
+				<view class="flex padding-lg">
+					<view class="text-content" style="letter-spacing: 0.1rem;">
+						<!-- <view class="margin-top-lg text-xl text-white">dddd</view> -->
+						<text :user-select="true">
+							&nbsp;&nbsp;如果你恰好有以上互联网业务开发需求，如果你想要一款量身定制的移动应用，如果你有新互联网项目计划，如果你恰好不知该怎么实行？
+							不妨来试试这里，首先我们是一对一式专享服务，假如你是小白，我会分析你提出的互联网项目可行性，我会告诉你该项目最贴近现实的互联网环境，
+							我会告诉你一切你最需要知道甚至你不想听到的内容，我会尽力解答你所有的疑惑，并给出可行方案；
+						</text>
+						<view class="margin-top-sm">
+							&nbsp;&nbsp; 那话不多说，先看价格：
 						</view>
 					</view>
 				</view>
-			</view>
-			<view class="cu-coupon-box2 radius-lg text-center align-center justify-center"
-				style="height: 60px;margin-top: -10px;">
-				<view class="flex align-center" style="height: 100%;">
-					<view class="flex-sub align-center">
-						<view class="grid col-1">
-							<view class="text-pink">
-								网站应用:
+				<view class="cu-coupon-box2 text-center align-center justify-center"
+					style="height: 60px;margin-top: -10px;">
+					<view class="flex align-center" style="height: 100%;">
+						<view class="flex-sub align-center">
+							<view class="grid col-1">
+								<view class="text-blue">
+									微信小程序:
+								</view>
 							</view>
 						</view>
-					</view>
-					<view class="flex-twice text-left">
-						<view class="grid col-1">
-							<view class="text-sm text-orange">
-								高效·高能·1百至5千元之间视需求不等；
+						<view class="flex-twice text-left">
+							<view class="grid col-1">
+								<view class="text-sm text-green">
+									简便·轻量·1百至8千元之间视需求不等；
+								</view>
 							</view>
 						</view>
 					</view>
 				</view>
-			</view>
-			<view class="cu-coupon-box2 text-center align-center justify-center"
-				style="height: 60px;margin-top: -10px;">
-				<view class="flex align-center" style="height: 100%;">
-					<view class="flex-sub align-center">
-						<view class="grid col-1">
-							<view class="text-orange">
-								APP软件:
+				<view class="cu-coupon-box2 radius-lg text-center align-center justify-center"
+					style="height: 60px;margin-top: -10px;">
+					<view class="flex align-center" style="height: 100%;">
+						<view class="flex-sub align-center">
+							<view class="grid col-1">
+								<view class="text-pink">
+									网站应用:
+								</view>
+							</view>
+						</view>
+						<view class="flex-twice text-left">
+							<view class="grid col-1">
+								<view class="text-sm text-orange">
+									高效·高能·1百至5千元之间视需求不等；
+								</view>
 							</view>
 						</view>
 					</view>
-					<view class="flex-twice text-left">
-						<view class="grid col-1">
-							<view class="text-sm text-pink">
-								高效·简便·5百至1万元之间视需求不等；
+				</view>
+				<view class="cu-coupon-box2 text-center align-center justify-center"
+					style="height: 60px;margin-top: -10px;">
+					<view class="flex align-center" style="height: 100%;">
+						<view class="flex-sub align-center">
+							<view class="grid col-1">
+								<view class="text-orange">
+									APP软件:
+								</view>
+							</view>
+						</view>
+						<view class="flex-twice text-left">
+							<view class="grid col-1">
+								<view class="text-sm text-pink">
+									高效·简便·5百至1万元之间视需求不等；
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
+				<view class="flex padding-lr-lg">
+					<view class="text-content" style="letter-spacing: 0.1rem;">
+						<text :user-select="true">
+							&nbsp;&nbsp;如果你嫌太花哨，太麻烦，那么可以直接跟我说，帮忙做个几百元的小程序，为哪些群体(或年龄段)使用，用来干什么，我会构思好一切，再反馈给你，
+							是不是像个AI，对呀，如果我们人类能像AI一样只反馈最真实最有效的信息，那我们是不是远远超越AI了呢？只要有信任存在，人一定比AI更真实，更高效，更方便，更可靠！
+						</text>
+						<view class="margin-top-sm">
+							&nbsp;&nbsp;由于是一对一式专享服务，及对服务内容与代码质量的保证，所以我可能会看需求接单，如果有数据体量特别大的项目，我会向你推荐一些可选的优质企业，因为我个人能力与时间毕竟有限，
+							开发周期过于漫长的话，对我们双方都不太好，望理解！
+						</view>
+						<view class="margin-top-sm">
+							&nbsp;&nbsp;因主职原因，还有一个硬性条件，就是不论项目体量大小，每个月我只承接最多两个项目，我的原则是，把每个对接的项目都以最优的方案编写，价格只是对时间的交换，
+							代码是对事业的尊重，不论是百元千元还是万元，代码质量都将以最高质量编写，并将源代码全权交付。另外，我卖的不是时间更不是代码，而是服务，是为人民服务的服务。
+						</view>
+						<view class="margin-top-sm text-center aqi-textShadow-wh">
+							&nbsp;&nbsp;先交付，后收费。不满意，无责退！
+						</view>
+						<view class="text-center padding-top-xs">
+							<view class="aqi-txtJinColor">
+								商务合作🤝<text class="cuIcon-weixin bg-gradual-green-light padding-xs round">Blue00008B</text>
 							</view>
 						</view>
 					</view>
 				</view>
 			</view>
-			<view class="flex padding-lr-lg">
-				<view class="text-content" style="letter-spacing: 0.1rem;">
-					<text :user-select="true">
-						&nbsp;&nbsp;如果你嫌太花哨，太麻烦，那么可以直接跟我说，帮忙做个几百元的小程序，给谁谁谁使用，用来干什么，我会构思好一切，再反馈给你，
-						是不是像个AI，对呀，如果我们人类能像AI一样只反馈最真实最有效的信息，那我们是不是远远超越AI了呢？只要有信任存在，人一定比AI更真实，更高效，更方便，更可靠！
-					</text>
-					<view class="margin-top-sm">
-						&nbsp;&nbsp;由于是一对一式专享服务，及对服务内容与代码质量的保证，所以我可能会看需求接单，如果有数据体量特别大的项目，我会向你推荐一些可选的优质企业，因为我个人能力与时间毕竟有限，
-						开发周期过于漫长的话，对我们双方都不太好，望理解！
-					</view>
-					<view class="margin-top-sm">
-						&nbsp;&nbsp;因主职原因，还有一个硬性条件，就是不论项目体量大小，每个月我只承接最多两个项目，我的原则是，把每个对接的项目都以最优的方案编写，价格只是对时间的交换，
-						代码是对事业的尊重，不论是百元千元还是万元，代码质量都将以最高质量编写，并将源代码全权交付。另外，我卖的不是时间更不是代码，而是服务，是为人民服务的服务。
-					</view>
-					<view class="text-center padding-top-xs">
-						<view class="aqi-txtJinColor">
-							商务合作🤝<text class="cuIcon-weixin bg-gradual-green-light padding-xs round">Blue00008B</text>
-						</view>
-					</view>
-				</view>
-			</view>
+			
 		</comNavTab>
 	</view>
 </template>
